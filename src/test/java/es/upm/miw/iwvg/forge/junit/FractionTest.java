34d315272ebdee8f046f8749b248ad962f37902d
jliriano <jd.liriano@alumnos.upm.es>
2018-10-03 22:53:18 +0200

    add test for method isPropia in FractionTest - issue #04

--- src/test/java/es/upm/miw/iwvg/forge/junit/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg/forge/junit/FractionTest.java
@@ -24,5 +24,10 @@ class FractionTest {
         assertEquals(2, fraction.decimal());
     }
 
+    @Test
+    void testIsPropia(){
+        this.fraction = new Fraction(1,3);
+        assertEquals(true,this.fraction.isPropia());
+    }
 
 }
